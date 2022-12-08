7805da432621908170dea9368d6c095d028521f8
nibir-code <mubtashimfuadnibir@gmail.com>
2022-12-08 18:58:32 +0600

    add-footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Login from "./Pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <Navbar></Navbar>
       <Routes>
          <Route path="/" element={<Home />}/>
